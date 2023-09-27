d043666d0c782182f44bde358d2f8b14e11a127b
Iva Damyanova <145503831+idamyanova@users.noreply.github.com>
2023-09-27 16:54:46 +0300

    Add files via upload

---
+++ Java Basics/RectangleArea.java
@@ -0,0 +1,19 @@
+package FirstStepsInCoding.Veronika;
+
+import java.util.Scanner;
+
+public class RectangleArea {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+                int firstNumber = Integer.parseInt(scanner.nextLine());
+                        int secondNumber = Integer.parseInt(scanner.nextLine());
+
+                                int area = firstNumber * secondNumber;
+
+                                        System.out.println(area);
+
+    }
+}
